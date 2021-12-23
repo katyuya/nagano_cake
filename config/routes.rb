9b94dc176198051c7b7aac28986806c4c08e2ddb
EC2 Default User <ec2-user@ip-172-31-4-37.ec2.internal>
2021-12-23 11:00:28 +0000

    [fix]ルートの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
   devise_for :customers
+
+
+  root to: 'homes#top'
+  get '/about' => 'homes#about'
+  resources :customers
+  resources :items, only:[:index, :show]
+
+
+  namespace :admin do
+    resources :customers
+    resources :items
+  end
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
 
 end
