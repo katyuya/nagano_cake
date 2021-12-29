57b344c537c35a4e45063b6488f8526fa4d503dc
EC2 Default User <ec2-user@ip-172-31-4-37.ec2.internal>
2021-12-29 18:08:10 +0000

    [add]ジャンル一覧の完成と商品の新規投稿の作成

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -12,10 +12,13 @@ class Admin::GenresController < ApplicationController
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
   end
+
+  private
 
   def genre_params
     params.require(:genre).permit(:name)
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
+
+  attachment :image
 end
