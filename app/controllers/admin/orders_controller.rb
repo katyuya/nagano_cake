b1bec0700db21c3082cc0d3f2544f3cc2b6a4fd5
EC2 Default User <ec2-user@ip-172-31-4-37.ec2.internal>
2021-12-30 19:58:07 +0000

    [add]会員一覧と詳細の編集の作成と注文履歴一覧の作成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,9 @@
-class Admin::OrderController < ApplicationController
+class Admin::OrdersController < ApplicationController
+
+  def index
+    @orders = Order.page(params[:page]).reverse_order
+  end
+
   def show
   end
 
