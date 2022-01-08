852a599f551cce798dbdec91e2c9e7e4800f2ce9
EC2 Default User <ec2-user@ip-172-31-4-37.ec2.internal>
2022-01-08 21:09:05 +0000

    [fix]マイページ編集と注文情報入力画面のエラーと追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,22 @@
 class OrdersController < ApplicationController
   def new
     @customer = current_customer
-    @address = current_customer.address.id
+    @address = current_customer.address
     @order = Order.new
   end
 
   def confirmation
+    @order = Order.new(order_params)
+    @order.save
+    redirect_to orders_confirmation_path
+
   end
 
   def thanks
   end
 
   def create
+
   end
 
   def index
@@ -20,4 +25,9 @@ class OrdersController < ApplicationController
   def show
   end
 
+  private
+
+  def order_params
+    params.require(:order).permit(:customer_id, :shipping_zip_code, :shipping_address, :delivery_name, :payment_method)
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,5 +3,6 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details, dependent: :destroy
 
+  enum address:[:"ご自身の住所", :"登録済みの住所から", :"新しいお届け先"]
   enum payment_method: { credit_card: 0, transfer: 1 }
 end
