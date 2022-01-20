4c9247294d0c6d19aee315443ff49c2677401236
EC2 Default User <ec2-user@ip-172-31-4-37.ec2.internal>
2022-01-20 15:21:45 +0000

    [add]退会のアラートの追加

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,5 +1,6 @@
 class Admin::SessionsController < ApplicationController
   before_action :authenticate_admin!
+
   def new
   end
 
@@ -7,5 +8,14 @@ class Admin::SessionsController < ApplicationController
   end
 
   def destroy
+  end
+
+  protected
+  def after_sign_in_path_for(resource)
+    admin_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
   end
 end
