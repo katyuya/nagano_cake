44ddd9f248d10461d4b38be9b416de34855bf47f
EC2 Default User <ec2-user@ip-172-31-4-37.ec2.internal>
2022-01-25 14:03:12 +0000

    [fix]すべて製作完了にしたときに注文ステータスを発送準備中にするif文の修正

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,12 +2,13 @@ class Admin::OrderDetailsController < ApplicationController
   before_action :authenticate_admin!
   def update
     @order_detail = OrderDetail.find(params[:id])
+    @order = @order_detail.order
     @order_detail.update(making_status:params[:order_detail][:making_status])
     if @order_detail.making_status == "producing"
       @order_detail.order.update(received: 2)
-    elsif @order_detail.making_status == "preparing"
+    elsif @order.order_details.all?{|order_detail| order_detail.making_status == "preparing"}
       @order_detail.order.update(received: 3)
     end
-    redirect_to admin_path
+    redirect_to admin_order_path(@order.id)
   end
 end
